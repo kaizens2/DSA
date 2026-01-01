7fd9aa42d69f4f6ff10b0edc01e3f91dd9f6ad81
ronak <chaudharyronak291@gmail.com>
2026-01-01 18:02:56 +0530

    Implement duplicate element detection in array with two methods

--- Leetcode/IsDuplicateElementInArray.java
+++ Leetcode/IsDuplicateElementInArray.java
@@ -1,12 +1,33 @@
 package Leetcode;
 
-public class IsDuplicateElementInArray {
-    public static void main() {
-        int[] arr = {1,2,3,4,5,6,7,8,9};
+import java.util.*;
 
+public class IsDuplicateElementInArray {
+    static void main() {
+        int[] arr = {1,2,3,4,5,6,7,8,9,1};
+        System.out.println(isDuplicate(arr));
+        System.out.println(isDuplicate2(arr));
     }
 
-    public boolean isDuplicate(int[] nums) {
-        return true;
+    public static boolean isDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            if(map.containsKey(num)){
+                return true;
+            }
+            map.put(num, 1);
+        }
+        return false;
+    }
+
+    // faster than MAP
+    public static boolean isDuplicate2(int[] nums) {
+       Set<Integer> set = new HashSet<>();
+       for(int num : nums){
+           if(!set.add(num)){
+               return true;
+           }
+       }
+        return false;
     }
 }
