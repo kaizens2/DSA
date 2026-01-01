318407f9ca663b8d9e336f40f0f493dad8a7a6da
ronak <chaudharyronak291@gmail.com>
2026-01-01 17:39:32 +0530

    Add initial project setup with .gitignore and module configuration files

---
+++ Leetcode/IsDuplicateElementInArray.java
@@ -0,0 +1,12 @@
+package Leetcode;
+
+public class IsDuplicateElementInArray {
+    public static void main() {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+
+    }
+
+    public boolean isDuplicate(int[] nums) {
+        return true;
+    }
+}
